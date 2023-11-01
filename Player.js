18a1c4075c501ed7a4ba6e40e6327078760f6e67
Pinarello45 <80649748+Pinarello45@users.noreply.github.com>
2023-11-02 10:27:12 +1300

    switch sprite key

--- Player.js
+++ Player.js
@@ -26,4 +26,44 @@ class Player extends Sprite {
         height: 10,
       }
   
-      
+      this.animations = animations
+      this.lastDirection = 'right'
+  
+      for (let key in this.animations) {
+        const image = new Image()
+        image.src = this.animations[key].imageSrc
+  
+        this.animations[key].image = image
+      }
+  
+      this.camerabox = {
+        position: {
+          x: this.position.x,
+          y: this.position.y,
+        },
+        width: 200,
+        height: 80,
+      }
+    }
+  
+    switchSprite(key) {
+      if (this.image === this.animations[key].image || !this.loaded) return
+  
+      this.currentFrame = 0
+      this.image = this.animations[key].image
+      this.frameBuffer = this.animations[key].frameBuffer
+      this.frameRate = this.animations[key].frameRate
+    }
+  
+    updateCamerabox() {
+      this.camerabox = {
+        position: {
+          x: this.position.x - 50,
+          y: this.position.y,
+        },
+        width: 200,
+        height: 80,
+      }
+    }
+  
+   
