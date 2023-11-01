ab7703aaea917c167c70ed40a0b9adaacfe6a4f6
Pinarello45 <80649748+Pinarello45@users.noreply.github.com>
2023-11-02 10:31:26 +1300

    check for horizontal collisions

--- Player.js
+++ Player.js
@@ -157,4 +157,48 @@ class Player extends Sprite {
       this.checkForVerticalCollisions()
     }
   
+    updateHitbox() {
+      this.hitbox = {
+        position: {
+          x: this.position.x + 35,
+          y: this.position.y + 26,
+        },
+        width: 14,
+        height: 27,
+      }
+    }
   
+    checkForHorizontalCollisions() {
+      for (let i = 0; i < this.collisionBlocks.length; i++) {
+        const collisionBlock = this.collisionBlocks[i]
+  
+        if (
+          collision({
+            object1: this.hitbox,
+            object2: collisionBlock,
+          })
+        ) {
+          if (this.velocity.x > 0) {
+            this.velocity.x = 0
+  
+            const offset =
+              this.hitbox.position.x - this.position.x + this.hitbox.width
+  
+            this.position.x = collisionBlock.position.x - offset - 0.01
+            break
+          }
+  
+          if (this.velocity.x < 0) {
+            this.velocity.x = 0
+  
+            const offset = this.hitbox.position.x - this.position.x
+  
+            this.position.x =
+              collisionBlock.position.x + collisionBlock.width - offset + 0.01
+            break
+          }
+        }
+      }
+    }
+  
+   
