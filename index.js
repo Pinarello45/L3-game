b03c0f2fd0c77c1d8c8804b3ed5ecc2e6133807c
Pinarello45 <80649748+Pinarello45@users.noreply.github.com>
2023-11-01 23:05:00 +1300

    gravity done

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ class Player {
     update() {
         this.draw()
        this.position.y += this.velocity.y 
-       if (this.position.y + this.height)
+       if (this.position.y + this.height + this.velocity.y < canvas.height) 
        this.velocity.y += gravity
+      else this.velocity.y = 0
     }
 
 }
