05a29422dd786a8989d655fe302cca316cc1247d
Pinarello45 <80649748+Pinarello45@users.noreply.github.com>
2023-11-01 22:42:48 +1300

    2nd player coords

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ canvas.width = 1024
 canvas.height = 576
 
 class Player {
-    constructor(){
-        this.position = {
-            x: 0,
-            y: 0,
-       }
+    constructor(position){
+        this.position = position
     }
     draw() {
         c.fillStyle = 'red'
@@ -23,13 +20,21 @@ class Player {
 }
 
 
-const player = new Player()
+const player = new Player({
+    x: 0,
+    y: 0,
+})
+const player2 = new Player({
+    x: 300,
+    y: 100,
+})
 
 function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
+    player2.update()
 }
 
 animate()
