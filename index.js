72e87a7d3a9a34c300bb42920ae89fc5f7b8c963
Pinarello45 <80649748+Pinarello45@users.noreply.github.com>
2023-11-01 23:56:06 +1300

    const sprite background

--- index.js
+++ index.js
@@ -7,13 +7,20 @@ canvas.height = 576
 const gravity = 0.5
 
 class Sprite {
-    constructor(position){
+    constructor({position, imageSrc}) {
         this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc
     }
 
     draw() {
-      c.drawImage(this.image)
+       if (!this.image) return
+      c.drawImage(this.image, this.position.x, this.position.y)
 
+    }
+
+    update() {
+        this.draw()
     }
 }
 
@@ -62,6 +69,10 @@ const keys = {
         pressed: false,
       },
 }
+
+const background = new Sprite({
+    
+})
 
 function animate(){
     window.requestAnimationFrame(animate)
