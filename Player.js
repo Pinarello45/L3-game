4a0923c558d235f762b3d5506c4e26743e5fced7
Pinarello45 <80649748+Pinarello45@users.noreply.github.com>
2023-11-02 10:28:49 +1300

    should pan the camera every direction

--- Player.js
+++ Player.js
@@ -66,4 +66,60 @@ class Player extends Sprite {
       }
     }
   
+    checkForHorizontalCanvasCollision() {
+      if (
+        this.hitbox.position.x + this.hitbox.width + this.velocity.x >= 576 ||
+        this.hitbox.position.x + this.velocity.x <= 0
+      ) {
+        this.velocity.x = 0
+      }
+    }
+  
+    shouldPanCameraToTheLeft({ canvas, camera }) {
+      const cameraboxRightSide = this.camerabox.position.x + this.camerabox.width
+      const scaledDownCanvasWidth = canvas.width / 4
+  
+      if (cameraboxRightSide >= 576) return
+  
+      if (
+        cameraboxRightSide >=
+        scaledDownCanvasWidth + Math.abs(camera.position.x)
+      ) {
+        camera.position.x -= this.velocity.x
+      }
+    }
+  
+    shouldPanCameraToTheRight({ canvas, camera }) {
+      if (this.camerabox.position.x <= 0) return
+  
+      if (this.camerabox.position.x <= Math.abs(camera.position.x)) {
+        camera.position.x -= this.velocity.x
+      }
+    }
+  
+    shouldPanCameraDown({ canvas, camera }) {
+      if (this.camerabox.position.y + this.velocity.y <= 0) return
+  
+      if (this.camerabox.position.y <= Math.abs(camera.position.y)) {
+        camera.position.y -= this.velocity.y
+      }
+    }
+  
+    shouldPanCameraUp({ canvas, camera }) {
+      if (
+        this.camerabox.position.y + this.camerabox.height + this.velocity.y >=
+        432
+      )
+        return
+  
+      const scaledCanvasHeight = canvas.height / 4
+  
+      if (
+        this.camerabox.position.y + this.camerabox.height >=
+        Math.abs(camera.position.y) + scaledCanvasHeight
+      ) {
+        camera.position.y -= this.velocity.y
+      }
+    }
+  
    
