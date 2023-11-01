8346f6696ecdf5fc5d6a1ac162b9a34790d60cdd
Pinarello45 <80649748+Pinarello45@users.noreply.github.com>
2023-11-01 23:31:35 +1300

    key up event listener

--- index.js
+++ index.js
@@ -42,12 +42,24 @@ const player2 = new Player({
     y: 100,
 })
 
+const keys = {
+    d: {
+      pressed: false,
+    },
+    a: {
+        pressed: false,
+      },
+}
+
 function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     player2.update()
+
+    player.velocity.x = 0
+    if(keys.d.pressed) player.velocity.x = 1
 }
 
 animate()
@@ -55,14 +67,27 @@ animate()
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case'd':
-        player.velocity.x = 1
+        keys.d.pressed = true
         break
         case'a':
-        player.velocity.x = -1
+        keys.a.pressed = true
         break
         case'w':
         player.velocity.y = -15
         break
+        
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case'd':
+        keys.d.pressed = false
+        break
+        case'a':
+        keys.a.pressed = false
+        break
+        
         
     }
 })
