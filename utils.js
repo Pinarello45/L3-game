1eab764c14ab2304bb5e1fc87c32841c7d07a4c1
Pinarello45 <80649748+Pinarello45@users.noreply.github.com>
2023-11-02 16:34:10 +1300

    funal product

--- utils.js
+++ utils.js
@@ -1,13 +1,13 @@
-function collision({ object1, object2 }) {
+function collision({ object1, object2 }) { //start collision function
     return (
       object1.position.y + object1.height >= object2.position.y &&
       object1.position.y <= object2.position.y + object2.height &&
       object1.position.x <= object2.position.x + object2.width &&
       object1.position.x + object1.width >= object2.position.x
     )
-  }
+  }//end collision function
   
-  function platformCollision({ object1, object2 }) {
+  function platformCollision({ object1, object2 }) { //start platform collision function
     return (
       object1.position.y + object1.height >= object2.position.y &&
       object1.position.y + object1.height <=
@@ -15,4 +15,4 @@ function collision({ object1, object2 }) {
       object1.position.x <= object2.position.x + object2.width &&
       object1.position.x + object1.width >= object2.position.x
     )
-  }
+  } //end platform collision function
