225bca33994f57f2883864210038d17815869eec
Pinarello45 <80649748+Pinarello45@users.noreply.github.com>
2023-11-02 13:01:58 +1300

    player js done

--- Player.js
+++ Player.js
@@ -206,4 +206,58 @@ class Player extends Sprite {
       this.position.y += this.velocity.y
     }
   
-   
+    checkForVerticalCollisions() {
+      for (let i = 0; i < this.collisionBlocks.length; i++) {
+        const collisionBlock = this.collisionBlocks[i]
+  
+        if (
+          collision({
+            object1: this.hitbox,
+            object2: collisionBlock,
+          })
+        ) {
+          if (this.velocity.y > 0) {
+            this.velocity.y = 0
+  
+            const offset =
+              this.hitbox.position.y - this.position.y + this.hitbox.height
+  
+            this.position.y = collisionBlock.position.y - offset - 0.01
+            break
+          }
+  
+          if (this.velocity.y < 0) {
+            this.velocity.y = 0
+  
+            const offset = this.hitbox.position.y - this.position.y
+  
+            this.position.y =
+              collisionBlock.position.y + collisionBlock.height - offset + 0.01
+            break
+          }
+        }
+      }
+  
+      // platform collision blocks
+      for (let i = 0; i < this.platformCollisionBlocks.length; i++) {
+        const platformCollisionBlock = this.platformCollisionBlocks[i]
+  
+        if (
+          platformCollision({
+            object1: this.hitbox,
+            object2: platformCollisionBlock,
+          })
+        ) {
+          if (this.velocity.y > 0) {
+            this.velocity.y = 0
+  
+            const offset =
+              this.hitbox.position.y - this.position.y + this.hitbox.height
+  
+            this.position.y = platformCollisionBlock.position.y - offset - 0.01
+            break
+          }
+        }
+      }
+    }
+  }
